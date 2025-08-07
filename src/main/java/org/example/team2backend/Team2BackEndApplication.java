fdb86d4d57da9ae3587ae5db0e78c721b8038d77
bxxrxtop <bsd03291@gmail.com>
2025-08-07 18:48:35 +0900

    🚀chore : 프로젝트 세팅

---
+++ src/main/java/org/example/team2backend/Team2BackEndApplication.java
@@ -0,0 +1,13 @@
+package org.example.team2backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team2BackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team2BackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/team2backend/Team2BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.team2backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team2BackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
